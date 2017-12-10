c16a77d5000b7b19f8b191536ca5f7b70cc5b3ae
neitsaab <33900042+neitsaab@users.noreply.github.com>
2017-12-10 18:33:27 +0100

    Add files via upload

---
+++ Fourmi.java
@@ -0,0 +1,11 @@
+package model;
+
+public interface Fourmi {
+	
+	public int getDirection();
+	
+	public int getCouleur();
+	
+	//retourne la nouvelle orientation de la Fourmi
+	public int tourner(int couleur);
+}
